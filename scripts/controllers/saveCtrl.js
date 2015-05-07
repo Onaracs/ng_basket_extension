0d8e3d310402f2ed317b055987e3238073ae18a5
Onaracs <brendan.scarano@gmail.com>
2015-05-06 21:40:07 -0400

    saving links properly

--- scripts/controllers/saveCtrl.js
+++ scripts/controllers/saveCtrl.js
@@ -46,6 +46,7 @@ angular.module('SaveCtrl', [
   $scope.saveLink = function(basketID) {
 
     console.log(basketID);
+    console.log($scope.message);
 
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {ping: "Send Page Info"}, function(response) {
