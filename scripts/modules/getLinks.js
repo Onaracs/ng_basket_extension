ce5edc5ff62800ea5f10ba86927524a43aa1ea3a
Onaracs <brendan.scarano@gmail.com>
2015-06-11 17:52:04 -0400

    added factory for getting users inbox

--- scripts/modules/getLinks.js
+++ scripts/modules/getLinks.js
@@ -25,3 +25,26 @@ angular.module('getLinks', [])
   }
 
 }])
+
+.factory('usersInbox', ['$http', function($http) {
+
+  return function() {
+    var url = 'http://localhost:3000/ng_inbox_links'
+
+    var promise = $http({
+      url: url,
+      method: 'GET'
+    }).success(function(response) {
+
+      return response;
+
+    }).error(function(response) {
+
+      return {'status': false};
+
+    })
+
+    return promise
+  }
+
+}])
