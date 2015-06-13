74aec0ab77c601cbf81ec3905cab0aa84ce72c64
Onaracs <brendan.scarano@gmail.com>
2015-06-13 16:37:55 -0400

    updated save link function to save link properly with new params

--- scripts/controllers/saveCtrl.js
+++ scripts/controllers/saveCtrl.js
@@ -19,42 +19,73 @@ angular.module('SaveCtrl', ['siteInfo'])
     console.log(response);
     $scope.url = response.url;
     $scope.title = response.title;
+    $scope.image = response.favIconUrl;
 
   })
 
   $scope.saveLink = function(basketID) {
 
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {ping: "Send Page Info"}, function(response) {
+    console.log($scope);
 
-        pageInfo = response.page_info;
-        console.log(pageInfo);
+    var promise = $http({
+      url: 'http://localhost:3000/new_link',
+      dataType: 'json',
+      method: 'POST',
+      params: {
+        url: $scope.url,
+        title: $scope.title,
+        message: $scope.message,
+        description: '',
+        image: $scope.image,
+        uniqueId: basketID
+      },
+      headers: {'Content-Type': 'application/json'}
+    }).success(function(response) {
 
-        var promise = $http({
-          url: 'http://localhost:3000/sent_link',
-          dataType: 'json',
-          method: 'POST',
-          params: {
-            url: tabUrl,
-            title: pageInfo.title,
-            message: $scope.message,
-            description: pageInfo.description,
-            image: pageInfo.info,
-            uniqueId: basketID
-          },
-          headers: {'Content-Type': 'application/json'}
-        }).success(function(response) {
-          console.log(response);
-          return response;
+      console.log(response);
+      return response;
 
-        }).error(function(response) {
+    }).error(function(response) {
 
-          return {'status': false};
+      return {'status': false};
 
-        })
-        
-      });
-    }); // chrome.tabs.query
+    })
+    
   } // saveLink()
 
 }])
+
+// $scope.saveLink = function(basketID) {
+
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     chrome.tabs.sendMessage(tabs[0].id, {ping: "Send Page Info"}, function(response) {
+
+//       pageInfo = response.page_info;
+//       console.log(pageInfo);
+
+//       var promise = $http({
+//         url: 'http://localhost:3000/sent_link',
+//         dataType: 'json',
+//         method: 'POST',
+//         params: {
+//           url: tabUrl,
+//           title: pageInfo.title,
+//           message: $scope.message,
+//           description: pageInfo.description,
+//           image: pageInfo.info,
+//           uniqueId: basketID
+//         },
+//         headers: {'Content-Type': 'application/json'}
+//       }).success(function(response) {
+//         console.log(response);
+//         return response;
+
+//       }).error(function(response) {
+
+//         return {'status': false};
+
+//       })
+      
+//     });
+//   }); // chrome.tabs.query
+// } // saveLink()
