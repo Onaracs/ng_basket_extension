135b12189366748d8ec88c6652e813ab15455f60
Onaracs <brendan.scarano@gmail.com>
2015-06-15 17:34:24 -0400

    removed unnecessary variables and functions from friends controller - now moved to the share form directive

--- scripts/controllers/friendsCtrl.js
+++ scripts/controllers/friendsCtrl.js
@@ -1,6 +1,6 @@
 angular.module('FriendCtrl', [
   'getFriends',
-  // 'shareLinkForm'
+  'shareLinkForm'
 ])
 
 .controller('FriendCtrl', ['$scope', '$http', 'getUsersFriends', function(
@@ -8,16 +8,6 @@ angular.module('FriendCtrl', [
   $http,
   getUsersFriends
 ) {
-
-  var tabUrl,
-      pageInfo;
-
-  // get the current URL
-  chrome.tabs.getSelected(null, function(tab) {
-
-      tabUrl = tab.url;
-
-  });
 
   getUsersFriends().then(function(response) {
 
@@ -30,46 +20,6 @@ angular.module('FriendCtrl', [
 
     $scope.selectedFriend = friend;
 
-    $scope.showMessage = true;
-
-  }
-
-  $scope.saveLink = function(friend) {
-
-    console.log(friend);
-    console.log($scope.message);
-
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {ping: "Send Page Info"}, function(response) {
-
-        pageInfo = response.page_info;
-        console.log(pageInfo);
-
-        var promise = $http({
-          url: 'http://localhost:3000/sent_link',
-          dataType: 'json',
-          method: 'POST',
-          params: {
-            url: tabUrl,
-            message: $scope.message,
-            title: pageInfo.title,
-            description: pageInfo.description,
-            image: pageInfo.info,
-            uniqueId: friend.id
-          },
-          headers: {'Content-Type': 'application/json'}
-        }).success(function(response) {
-          console.log(response);
-          return response;
-
-        }).error(function(response) {
-
-          return {'status': false};
-
-        })
-        
-      });
-    }); // chrome.tabs.query
   }
 
 }])
