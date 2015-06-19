9f509e5a3c9ad2042cecfad752acfced2687e1c0
Onaracs <brendan.scarano@gmail.com>
2015-06-19 07:42:44 -0400

    added getBasketInfo factory to getBaskets module

--- scripts/modules/getBaskets.js
+++ scripts/modules/getBaskets.js
@@ -23,3 +23,28 @@ angular.module('getBaskets', [])
   }
 
 }])
+
+.factory('getBasketInfo', ['$http', function($http) {
+
+  var url = 'http://localhost:3000/ng_basket_info';
+  
+  return function(basketID) {
+    var promise = $http({
+      url: url,
+      params: {basketID: basketID},
+      method: 'GET'
+    }).success(function(response) {
+
+      console.log(response);
+      return response;
+
+    }).error(function(response) {
+
+      return {'status': false};
+
+    })
+
+    return promise;
+  }
+
+}])
